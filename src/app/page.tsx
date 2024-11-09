1f97db63ca20a5a7541b0af6e3fc987c5f989c92
Om-khairnar <om.khairnar175@gmail.com>
2024-11-09 19:06:04 +0530

    some .env error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-    {/* MONGODB_URI=mongodb+srv://Omkara2002:omkara2002@user-authentication.9qfy0.mongodb.net/
-    JWT_SECRETKEY=nisdjbfuhvshbvsbudhb */}
+      {/* MONGODB_URI=mongodb+srv://Omkara2002:omkara2002@user-authentication.9qfy0.mongodb.net/
+      JWT_SECRETKEY=nisdjbfuhvshbvsbudhb */}
     </>
   );
 }
